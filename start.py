87014f438062482247d968169427469e51b79f1e
t0theheart <35728736+t0theheart@users.noreply.github.com>
2020-06-24 18:20:49 +0300

    refactor start and add example of using

--- start.py
+++ start.py
@@ -2,7 +2,25 @@ from fibonacci_iterator import Fibonacci
 
 
 def main():
+    """
+    Example of using:
 
+    "Fibonacci iterator" started!
+    Enter index of first elem [default 0]: 0
+    Enter index of last elem [default 100]: 10
+    Enter index of elem where iteration starts [default 0]: 5
+    Enter "+" or "-" to iter next or previous fibonacci value. "exit" to end program.
+    -> +
+    8
+    -> +
+    13
+    -> -
+    8
+    -> -
+    5
+    -> exit
+    End.
+    """
     def _do_command(_f, _command):
         try:
             if command == '+':
@@ -20,15 +38,15 @@ def main():
     last = int(input('Enter index of last elem [default 100]: ') or 100)
     start = int(input('Enter index of elem where iteration starts [default 0]: ') or 0)
 
-    f = Fibonacci(first=first, last=last, start=start)
-    right_iterator = f.right_order_iterator()
-    reverse_iterator = f.reverse_order_iterator()
+    fib = Fibonacci(first=first, last=last, start=start)
+    right_iterator = fib.right_order_iterator()
+    reverse_iterator = fib.reverse_order_iterator()
 
     print('Enter "+" or "-" to iter next or previous fibonacci value. "exit" to end program.')
 
     while True:
         command = input('-> ')
-        _do_command(f, command)
+        _do_command(fib, command)
 
 
 if __name__ == '__main__':
